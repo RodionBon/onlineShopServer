fdf711481e143de9a0e7538d7bf6b970519ef69b
Rodion <rodionb4649@gmail.com>
2025-11-05 23:43:25 +0200

    feat: added endpoint to receive products

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,56 @@
+import { Context } from "koa";
+import { Product } from "../models";
+import { Op, Order, Sequelize, WhereOptions } from "sequelize";
+
+export const getFilteredProducts = async (ctx: Context) => {
+    try {
+        const { searchQuery, maxPrice, minPrice, isShowOnlyAvailable, filterType, itemsPerPage, pageNumber } = ctx.query;
+
+        const whereOptions: WhereOptions = {};
+
+        if (searchQuery) {
+            whereOptions.title = { [Op.like]: `%${searchQuery}%` };
+        }
+
+        if (minPrice && maxPrice) {
+            whereOptions.price = { [Op.between]: [minPrice, maxPrice] };
+        }
+
+        if (isShowOnlyAvailable) {
+            whereOptions.itemsLeft = { [Op.gt]: 0 };
+        }
+
+        let orderType: Order;
+        switch (filterType) {
+            case 'fromCheap':
+                orderType = [['price', 'ASC']];
+                break;
+            case 'fromExpensive':
+                orderType = [['price', 'DESC']];
+                break;
+            case 'fromZtoA':
+                orderType = [[Sequelize.fn('LOWER', Sequelize.col('title')), 'DESC']];
+                break;
+            case 'fromAtoZ':
+            default:
+                orderType = [[Sequelize.fn('LOWER', Sequelize.col('title')), 'ASC']];
+                break;
+        }
+
+        const products = await Product.findAll({
+            where: whereOptions,
+            order: orderType,
+            limit: Number(itemsPerPage),
+            offset: Number(pageNumber) * Number(itemsPerPage),
+        });
+        const count = await Product.count({ where: whereOptions });
+        const maxAvailablePrice = await Product.max('price');
+
+        ctx.status = 200;
+        ctx.body = { products, count, maxAvailablePrice };
+    } catch (error) {
+        console.error(error);
+        ctx.status = 500;
+        ctx.body = { error: 'Internale Serverfehler' };
+    }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import Router from "@koa/router";
+import ProductRouter from "./product.routes";
 
 const router = new Router();
 
 router.get('/health', async ctx => {
   ctx.body = "Alles ist in Ordnung!";
 });
+
+router.use('/product', ProductRouter.routes());
 
 export default router;
