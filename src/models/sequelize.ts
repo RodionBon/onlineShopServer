59e6a7216cc43ec1b825247c202d155491651359
Rodion <rodionb4649@gmail.com>
2025-12-09 15:00:31 +0200

    feat: changed .env setup, so that it takes full URL to DB

--- src/models/sequelize.ts
+++ src/models/sequelize.ts
@@ -1,8 +1,8 @@
 require('dotenv').config();
 import { Sequelize } from "sequelize";
 
-const { PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD } = process.env;
+const { DB_EXTERNAL_URL } = process.env;
 
-const sequelize = new Sequelize(`postgres://${PG_USER}:${PG_PASSWORD}@${PG_HOST}:${PG_PORT}/${PG_DATABASE}`);
+const sequelize = new Sequelize(DB_EXTERNAL_URL ?? "");
 
 export default sequelize;
